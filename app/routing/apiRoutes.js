6080fc1820fc6b229cf8c06e68415304604db0b5
William Blake <willblakebooking@gmail.com>
2018-02-05 11:10:55 -0600

    Added friends object

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-var friends = ('../app/data/friends');
+var friends = require('../data/friends');
 
 module.exports = function (app) {
 
@@ -7,7 +7,7 @@ module.exports = function (app) {
   });
 
   app.post('/api/friends', function (req, res) {
-
+      res.json(friends);
     });
 
   console.log(friends);
